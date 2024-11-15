c879b67b5e3b9d83aeaa2962d8f3ceb92e5f5f2a
Minenhle Ncapai <minipai64@gmail.com>
2024-11-15 12:18:54 +0200

    Set up App.js with Header and Meme components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 
-function App() {
 
-  return (
-    <>
-     
-    </>
-  )
+import React from "react"
+import Header from "./Components/Header.js"
+import Meme from "./Components/Meme.js"
+/**
+ * Challenge: 
+ * - Create a Meme component.
+ * - Inside the Meme component, render a styled form
+ *   with our 2 inputs and the button.
+ * - Don't worry about adding any functionality yet
+ */
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
 }
-
-export default App
